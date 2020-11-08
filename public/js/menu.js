b4e3f38ed2f41deed853dbfd917cd60c7cbcc049
root <toiwilouhassane@gmail.com>
2020-11-08 20:40:22 +0100

    add js to menu.js file

---
+++ public/js/menu.js
@@ -0,0 +1,20 @@
+
+(function(){
+
+    var scrollY = function () {
+        var supportPageOffset = window.pageXOffset !== undefined;
+        var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+        return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+    }
+    
+    var menu = document.querySelector('.menu')
+    var onScroll = function(){
+        if(scrollY() > 0){
+            menu.classList.add('fixed')
+        } else {
+            menu.classList.remove('fixed')
+        }
+    }
+    window.addEventListener('scroll', setInterval(onScroll, 500))
+
+})()
